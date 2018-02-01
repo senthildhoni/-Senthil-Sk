b7a9466e63abc9c6864c76d9d6e6227051518703
senthildhoni <35728590+senthildhoni@users.noreply.github.com>
2018-02-01 16:54:31 +0530

    Create helllo.c

---
+++ helllo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    
+
+    
+    printf("enter a hello");
+    for (i=1;i<=1;i++)
+    {
+        
+     for (j=1;j<=1;j++)
+        
+    printf ("hello");
+    }return 0;
+
+}
