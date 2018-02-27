83cb0256561e74d7a977619f381bacc5f83cf946
senthildhoni <35728590+senthildhoni@users.noreply.github.com>
2018-02-27 16:41:16 +0530

    Create 155.c

---
+++ 155.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+
+{
+
+int i;
+
+printf ("\n the even numbers are:");
+
+for (i=10;i<=20;i++)
+
+{
+
+if (i%2==0)
+
+printf ("%d",i);
+
+printf ("\n");
+
+}
+
+
+
+}
